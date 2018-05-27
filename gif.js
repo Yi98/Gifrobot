31ba0db4c1cc4c425fb14175eea7252b313798d9
Yi98 <ngyi07285@hotmail.com>
2018-05-27 21:32:50 +0800

    fetch url and title in api

--- gif.js
+++ gif.js
@@ -2,14 +2,17 @@ const request = require('request');
 
 var limit = 10;
 
-var address = `https://api.giphy.com/v1/gifs/trending?api_key=AH1lxNGJ7pZeEyEJlTQ6EY3RK0EFfPGZ&limit=${limit}&rating=G`;
+var trending = `https://api.giphy.com/v1/gifs/trending?api_key=AH1lxNGJ7pZeEyEJlTQ6EY3RK0EFfPGZ&limit=${limit}&rating=G`;
 
 var results = (callback) => {
-  request(address, {json: true}, (err, res, body) => {
+  request(trending, {json: true}, (err, res, body) => {
       if(!err) {
         var gifs = [];
         for(let i=0; i<limit; i++) {
-          gifs.push(body.data[i].images.original.url);
+          gifs.push({
+            url: body.data[i].images.original.url,
+            title: body.data[i].title
+          });
         }
         callback(undefined, {
           gifs
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header/header.component';
 import { DisplayComponent } from './home/display/display.component';
 import { HomeComponent } from './home/home.component';
-import { JumbotronComponent } from './home/header/jumbotron/jumbotron.component';
-import { ChatareaComponent } from './home/display/chatarea/chatarea.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     DisplayComponent,
-    HomeComponent,
-    JumbotronComponent,
-    ChatareaComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
