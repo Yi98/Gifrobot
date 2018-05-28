0d5302ac275f9db5d0d4d77ffcf87e3a6610cbe9
Yi98 <ngyi07285@hotmail.com>
2018-05-28 17:19:55 +0800

    random and stickers nav active works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/trending', pathMatch: 'full'},
+  { path: 'trending', component: HomeComponent },
+  { path: 'random', component: HomeComponent },
+  { path: 'stickers', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
